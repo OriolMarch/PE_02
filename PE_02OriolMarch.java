8afc9b2352dc0f7ad23a9500431f1e210bbece74
oriolmarch <icewoo2001@gmail.com>
2025-10-04 18:26:05 +0200

    Reestructuració de codi + Decisió 2 pdarr

--- PE_02OriolMarch.java
+++ PE_02OriolMarch.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class PE_02OriolMarch {
     public static void main(String[] args) { 
 
-Scanner sc = new Scanner(System.in);
+  Scanner sc = new Scanner(System.in);
 
-      //Decisió 0
+      
         System.out.println("Decisió 0: Que fas?");
         System.out.println("1) Agafar la Bíblia i entrar");
         System.out.println("2) Entrar directament");
         System.out.print("Opció: ");
         int d0 = sc.nextInt();
 
-  boolean teBiblia;
+        boolean teBiblia;
         if (d0 == 1) {
             teBiblia = true;
         } else {
@@ -21,8 +21,9 @@ Scanner sc = new Scanner(System.in);
         }
 
         System.out.println("\nEstàs davant de la porta principal.");
+
     
-               // Decisió 1
+        // Decisió 1
         
         System.out.println("Decisió 1: Entrar per la porta principal o per la porta de darrere?");
         System.out.println("1) Porta principal");
@@ -30,12 +31,12 @@ Scanner sc = new Scanner(System.in);
         System.out.print("Opció: ");
         int d1 = sc.nextInt();
 
-                switch (d1) {
-            case 1: {}
+        switch (d1) {
+            case 1: {
                 // Porta principal
                 System.out.println("\nHas entrat per la porta principal.");
                 
-                // Decisió 2 
+                // Decisió 2 ( true porta principal)
                 
                 System.out.println("Decisió 2: Vas cap a la planta superior o soterrani?");
                 System.out.println("1) Planta superior");
@@ -48,7 +49,7 @@ Scanner sc = new Scanner(System.in);
                         // Planta superior
                         System.out.println("\nPlanta superior: Veus una persona vella i amb una forma molt estranya.");
                     
-                        // Decisió 3
+                        // Decisió 3 (planta superior)
                         
                         System.out.println("Decisió 3: L’ajudes o no l’ajudes?");
                         System.out.println("1) Ajudar");
@@ -70,9 +71,45 @@ Scanner sc = new Scanner(System.in);
                     case 2: {
                         // Soterrani
                         System.out.println("\nSoterrani: Està tot fosc i hi ha un home amb una destral.");
-    }
+                        
+                        // Decisió 3 (soterrani)
+    
+                        System.out.println("Decisió 3: Afrontar-te amb ell o escapar?");
+                        System.out.println("1) Afrontar");
+                        System.out.println("2) Escapar");
+                        System.out.print("Opció: ");
+                        int d3_soterrani = sc.nextInt();
 
-}
-}
+                        if (d3_soterrani == 1) {
+                            if (teBiblia) {
+                                System.out.println("\nAfrontar amb Bíblia: Tens una creu a la Bíblia, fas un exorcisme i desapareix. (Final Feliç)");
+                            } else {
+                                System.out.println("\nAfrontar sense Bíblia: Vences l'home, però mors per hemorràgia. (Final Tràgic)");
+                            }
+                        } else {
+                            System.out.println("\nEscapar: Penses que escapas, però en pujar hi ha el dimoni i et posseeix. (Final Dolent)");
+                        }
+                        break;
+                    }
+                    default: {
+                        System.out.println("\nOpció no vàlida a la Decisió 2.");
+                        break;
+                    }
+                }
+                break;
+            }
+            case 2: {
+                // Porta de darrere
+                System.out.println("\nHas entrat pel darrere i trobes una porta-portal a un altre univers.");
+                
+                // Decisió 2 (porta darrere)
+                
+                System.out.println("Decisió 2: Què fas?");
+                System.out.println("1) Entrar al portal");
+                System.out.println("2) Tornar a la porta principal");
+                System.out.print("Opció: ");
+                int d2_darrere = sc.nextInt();
+        }
+    }
     }
 }
