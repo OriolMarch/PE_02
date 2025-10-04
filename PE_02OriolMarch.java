972e7759d5f265bcbd8067a9992e78bbd15a33c5
oriolmarch <icewoo2001@gmail.com>
2025-10-04 18:03:41 +0200

    Millor estructurat + Decisió 1/2

--- PE_02OriolMarch.java
+++ PE_02OriolMarch.java
@@ -1,3 +1,9 @@
+
+import java.util.Scanner;
+
+public class PE_02OriolMarch {
+    public static void main(String[] args) { 
+
 Scanner sc = new Scanner(System.in);
 
       //Decisió 0
@@ -6,3 +12,36 @@ Scanner sc = new Scanner(System.in);
         System.out.println("2) Entrar directament");
         System.out.print("Opció: ");
         int d0 = sc.nextInt();
+
+  boolean teBiblia;
+        if (d0 == 1) {
+            teBiblia = true;
+        } else {
+            teBiblia = false;
+        }
+
+        System.out.println("\nEstàs davant de la porta principal.");
+    
+               // Decisió 1
+        
+        System.out.println("Decisió 1: Entrar per la porta principal o per la porta de darrere?");
+        System.out.println("1) Porta principal");
+        System.out.println("2) Porta de darrere");
+        System.out.print("Opció: ");
+        int d1 = sc.nextInt();
+
+                switch (d1) {
+            case 1: {}
+                // Porta principal
+                System.out.println("\nHas entrat per la porta principal.");
+                
+                // Decisió 2 
+                
+                System.out.println("Decisió 2: Vas cap a la planta superior o soterrani?");
+                System.out.println("1) Planta superior");
+                System.out.println("2) Soterrani");
+                System.out.print("Opció: ");
+                int d2_principal = sc.nextInt();
+    }
+}
+}
