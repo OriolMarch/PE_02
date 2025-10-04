e3c8fcf7cdd4f22ea17996ff533c7397afc72ee4
oriolmarch <icewoo2001@gmail.com>
2025-10-04 17:50:33 +0200

    Creació fitxer + Decisió 0

---
+++ PE_02OriolMarch.java
@@ -0,0 +1,8 @@
+Scanner sc = new Scanner(System.in);
+
+      //Decisió 0
+        System.out.println("Decisió 0: Que fas?");
+        System.out.println("1) Agafar la Bíblia i entrar");
+        System.out.println("2) Entrar directament");
+        System.out.print("Opció: ");
+        int d0 = sc.nextInt();
